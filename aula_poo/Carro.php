1d765103a34a3f1c6dba3391de837bffd41814f1
BielHtml <95312130+BielHtml@users.noreply.github.com>
2023-08-03 16:26:16 -0300

    Add files via upload

---
+++ aula_poo/Carro.php
@@ -0,0 +1,24 @@
+<?php
+class Carro{
+    public $marca;
+    public $modelo;
+    public $ano;
+
+    function __construct($marca,$modelo,$ano)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->ano = $ano;
+
+    }
+
+    function get_marca(){
+        return $this->marca ;
+    }
+    function get_modelo(){
+        return $this->modelo ;
+    }
+    function get_ano(){
+        return $this->ano;
+    }
+}

b40c6828493120a75aa4321b19c70764f5b12a74
BielHtml <95312130+BielHtml@users.noreply.github.com>
2023-08-03 15:10:53 -0300

    Add files via upload

---
+++ aula_poo/index.php
@@ -0,0 +1,41 @@
+<?php
+class Caio {
+public $nome; 
+ public $idade; 
+ public $tamanho;
+ public $peso;
+
+  /* function set_idade ($idade){
+    $this->idade = $idade;
+ }
+ */
+function __construct($nome,$idade)
+{
+ $this->nome = $nome;
+ $this->idade = $idade;
+
+}
+ function get_idade (){
+    return $this->idade;
+ }
+
+ /* public function set_nome ($nome){
+    $this->nome = $nome; 
+ }
+ */
+ public function get_nome (){
+    return $this->nome;
+ }
+}
+
+$pessoa = new caio("caio",18);
+
+
+
+$pessoa2 = new caio("jose", 20);
+
+
+echo $pessoa->get_nome(). " tem: ".$pessoa->get_idade()." anos"."</br>";  
+echo $pessoa2->get_nome(). " tem: ".$pessoa2->get_idade()." anos"."</br>";  
+
+?>
